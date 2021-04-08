4a66b2cc038f3a48893060f641af0d2d9ce42a8c
David Siddle <siddav83@gmail.com>
2021-04-08 13:48:13 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
+
+  resources :ebikes, only: %w[index show new create destroy update] do
+    # resources :bookings, only: %w[index show new create delete update]
+    resources :bookings, only: %w[new create]
+  end
+  # test to see what
+  resources :bookings, only: %w[index destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
